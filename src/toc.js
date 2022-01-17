c790c8f6827f1e6b4d53b9c3196300786643af93
Antonio <zolotuhin87@gmail.com>
2022-01-17 02:46:17 +0200

    feat(core): add indents for nested headings in toc

--- src/toc.js
+++ src/toc.js
@@ -1,8 +1,9 @@
 let buildToc = (htmlContent) => {
   let tocHeadings = [];
-  let results = [...htmlContent.matchAll(/<h[1-6] id="(?<link>.*)">(?<name>.*)<\/h[1-6]>/g)];
+  let results = [...htmlContent.matchAll(/<h(?<id>[2-6]) id="(?<link>.*)">(?<name>.*)<\/h[2-6]>/g)];
   results.map(item => {
     tocHeadings.push({
+      id: item.groups.id,
       link: item.groups.link,
       title: item.groups.name
     });
