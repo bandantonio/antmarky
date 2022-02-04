8b4823b59f0b4a09dedf174474122ab3a9878d1b
Antonio <zolotuhin87@gmail.com>
2022-02-05 01:55:45 +0200

    feature: added support for remore markdown files from git repos

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,10 +1,12 @@
-let { findMdFiles, getFilesContent, convertMdToHtml } = require('../common/prepare-content')
+let { findMdFiles, getFilesContent, convertMdToHtml } = require('../common/prepare-content');
+let { embedRemoteMarkdown } = require('../common/embed-remote-markdown');
 
 let serveContent = async (req, res, next) => {
   let locatedMdFiles = await findMdFiles();
   let allPages = locatedMdFiles;
-  let filesMdContent = await getFilesContent(locatedMdFiles);
-  let htmlContent = convertMdToHtml(filesMdContent);
+  let mdFilesContent = await getFilesContent(locatedMdFiles);
+  let mdFilesWithRemoteContent = await embedRemoteMarkdown(mdFilesContent);
+  let htmlContent = convertMdToHtml(mdFilesWithRemoteContent);
   res.locals.files_data = htmlContent;
   res.locals.all_pages = allPages;
   next();
