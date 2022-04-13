5d3f94387992416b31924f187a000801cf5ab3f8
Antonio <zolotuhin87@gmail.com>
2022-04-14 02:33:23 +0300

    refactor(core): improve code to rise test coverage

--- test/unit/findMdFiles.test.js
+++ test/unit/findMdFiles.test.js
@@ -1,27 +1,75 @@
 const chai = require('chai');
 const { expect } = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const mock = require('mock-fs');
 const { findMdFiles } = require('../../src/common/prepare-content');
 chai.use(chaiAsPromised);
 
 describe('module findMdFiles', () => {
-  it('Ensure the module works and returns a result', async () => {
+
+  it('Ensure the module outputs an array (default `docs` directory)', async () => {
     const files = await findMdFiles();
-    expect(files).to.not.be.undefined;
+    expect(files).to.be.an('array');
+  }).title
+
+  it.skip('Ensure the module outputs an array (custom `docs` directory)', async () => {
+    const files = await findMdFiles();
+    expect(files).to.be.an('array');
   });
 
-  it('Ensure the module outputs an array', async () => {
-    const files = await findMdFiles();
-    expect(files).to.be.an.instanceOf(Array);
+  it('Ensure the module outputs an array (root directory)', async () => {
+    const files = await findMdFiles('/');
+    expect(files).to.be.an('array');
   });
 
-  it('Throw an Error when docs directory name is invalid', async () => {
+  it('Ensure the module outputs an array of expected format (default `docs` directory)', async () => {
+    const files = await findMdFiles();
+    files.map(obj => (expect(obj).to.include.all.keys(['dirLevel', 'basePath', 'dirPath', 'dirClass', 'dirName', 'files'])))
+  });
+
+  it.skip('Ensure the module outputs an array of expected format (custom `docs` directory)', async () => {
+    const files = await findMdFiles();
+    files.map(obj => (expect(obj).to.include.all.keys(['dirLevel', 'basePath', 'dirPath', 'dirClass', 'dirName', 'files'])))
+  });
+
+  it('Ensure the module outputs an array of expected format (root directory)', async () => {
+    const files = await findMdFiles('/');
+    files.map(obj => (expect(obj).to.include.all.keys(['dirLevel', 'basePath', 'dirPath', 'dirClass', 'dirName', 'files'])))
+  });
+
+  it('Throw an error when docs directory name is invalid', async () => {
     const failedValue = 123;
     await expect(findMdFiles(failedValue)).to.be.rejectedWith('Invalid docs directory');
   });
 
-  it(`Throw an Error when docs directory doesn't exist`, async () => {
+  it(`Throw an error when the specified docs directory doesn't exist`, async () => {
     const failedDirectoryPath = 'nonexistentdirectory';
     await expect(findMdFiles(failedDirectoryPath)).to.be.rejectedWith('The specified directory does not exist');
+  });
+
+  describe.skip('module findMdFiles - working with files', () => {
+    it('Output files from the root docs directory', async () => {
+      mock({
+        'fake-root-one.md': `# Title\nHello world from the fake-root-one file`,
+        'fake-root-two.md': `# Title\nHello world from the fake-root-two file`,
+      });
+
+      let result = await findMdFiles('/');
+      expect(result.map(obj => obj.files)).to.deep.include(['fake-root-one', 'fake-root-two'])
+      mock.restore();
+    });
+    
+    it(`Output files from the default docs directory`, async () => {
+      mock({
+        'docs': {
+          'fake-docs-dir-one.md': `# Title\nHello world from the fake-docs-dir-one file`,
+          'fake-docs-dir-two.md': `# Title\nHello world from the fake-docs-dir-two file`,
+        }
+      });
+
+      let result = await findMdFiles();
+      expect(result).to.be.an('array').that.includes.ordered.members([ 'fake-docs-dir-one.md', 'fake-docs-dir-two.md' ]);
+      mock.restore();
+    });
   });
 });
--- test/unit/getFilesContent.test.js
+++ test/unit/getFilesContent.test.js
@@ -1,14 +1,47 @@
 const chai = require('chai');
 const { expect } = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const mock = require('mock-fs');
 const { getFilesContent } = require('../../src/common/prepare-content');
 chai.use(chaiAsPromised);
 
 describe('module getFilesContent', () => {
-  it('Ensure the module throws an error when passing input of incorrect type', async () => {
+  it('Throw an error when passing input of incorrect type', async () => {
     let inputOfIncorrectType = ['string', 77, { "name": "John" }];
     for (let value of inputOfIncorrectType) {
       await expect(getFilesContent(value)).to.be.rejectedWith(`Can't get content from Markdown files`);
     }
   });
+  
+  it('Get file details from default docs directory', async () => {
+    // Merge this test later with the one below
+    mock({
+      'docs': {
+        'unit-tests.md': `# Title\nHello world from the fake-root-one file\n[link to GitHub](https://github.com/bandantonio)`,
+        'markdown.md': `# Title\nHello world from the fake-root-two file\n[link to Antmarky](https://github.com/bandantonio/antmarky)`,
+      }
+    });
+    let content = [{ dirLevel: 0, basePath: `${process.cwd()}/docs`, dirPath: '', dirClass: 'home', dirName: 'Home', files: [ 'unit-tests', 'markdown' ] }]
+    
+    let result = await getFilesContent(content);
+
+    expect(result).length.to.be(2);
+    result.forEach(obj => expect(obj).to.include.all.keys('name', 'title', 'content'));
+    expect(result[0]).to.eql({
+      name: 'unit-tests',
+      title: 'unit-tests',
+      content: '# Title\n' + 'Hello world from the fake-root-one file\n' + '[link to GitHub](https://github.com/bandantonio)'
+    });
+    expect(result[1]).to.eql({
+      name: 'markdown',
+      title: 'markdown',
+      content: '# Title\n' + 'Hello world from the fake-root-two file\n' + '[link to Antmarky](https://github.com/bandantonio/antmarky)'
+    });
+
+    mock.restore();
+  });
+
+  it('Get file details from default docs directory')
+  it('Get file details from root directory')
+  it('Get file details from custom docs directory')
 });
