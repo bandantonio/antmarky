f0d3f008718ab2d1feb06b2177d3ceb79b150ae6
Antonio <zolotuhin87@gmail.com>
2022-05-28 13:58:52 +0300

    test: migrate to jest
    This update brings full switch to jest test runner instead of mocha
    - made full coverage with unit tests of the main flows
    - replaced axios calls to GitHub and BitBucket with mocks
    - updated source code to work properly with doc assets
    - removed chai and chai-promisified as no longer needed
    - removed istanbul as a separate package

--- test/unit/convertMdToHtml.test.js
+++ test/unit/convertMdToHtml.test.js
@@ -1,18 +1,14 @@
-const chai = require('chai');
-const { expect } = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 const { convertMdToHtml } = require('../../src/common/prepare-content');
-chai.use(chaiAsPromised);
 
 describe('module convertMdToHtml', () => {
-  it('Ensure the module throws an error when passing input of incorrect type', () => {
+  test('Ensure the module throws an error when passing input of incorrect type', () => {
     let inputOfIncorrectType = ['string', 77, { "name": "John" }];
     for (let value of inputOfIncorrectType) {
-      expect(() => convertMdToHtml(value)).to.throw('Can\'t convert. Input data is invalid');
+      expect(() => convertMdToHtml(value)).toThrowError('Can\'t convert. Input data is invalid');
     }
   });
 
-  it('convert content without remote inclusion', () => {
+  test('Convert content without remote inclusion', () => {
     let content = [{
       name: 'features',
       title: 'features',
@@ -24,9 +20,9 @@ describe('module convertMdToHtml', () => {
     }];
 
     let result = convertMdToHtml(content);
-    expect(result).to.have.length(2);
-    result.forEach(obj => expect(obj).to.include.all.keys('name', 'title', 'html', 'toc'));
+    expect(result).toHaveLength(2);
+    result.forEach(obj => expect(Object.keys(obj)).toEqual(['name', 'title', 'html', 'toc']));
   });
-  it.skip('convert content with remote inclusion')
-  it.skip('create mock function to serve remote content')
+  test.todo('convert content with remote inclusion');
+  test.todo('create mock function to serve remote content');
 });
