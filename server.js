e6fd1dbf228b4fc1c9d6402e3675b5abd26b35b4
Antonio <bandantonio@users.noreply.github.com>
2022-08-27 14:54:35 +0300

    perf: switch to Fastify instead of Express (#49)

--- server.js
+++ server.js
@@ -1,49 +1,80 @@
-const express = require('express');
+const fastify = require('fastify')({ logger: true });
 const fs = require('fs');
 const path = require('path');
-const app = express();
-const PORT = 8000;
-const { serveContent } = require('./src/commands/serve');
+const { findDocFiles, getFilesContent, convertDocToHtml } = require('./src/common/prepare-content');
 const { adoc, asciidoctorDefaultConfig } = require('./src/common/parsers');
 const { errorPage } = require('./src/data/defaults');
-app.use(serveContent);
+const fastifyStatic = require('@fastify/static');
+const PORT = 8000;
+const assetsDir = 'public';
+asciidoctorDefaultConfig.attributes.imagesdir = assetsDir;
 
-app.set('view engine', 'ejs');
-app.use(express.static('src/assets'));
-app.use(express.static('docs'));
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'src/assets')
+});
 
-app.get('/', (req, res) => {
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'docs'),
+  prefix: `/${assetsDir}`,
+  decorateReply: false
+});
+
+fastify.register(require('@fastify/view'), {
+  engine: {
+    ejs: require('ejs')
+  },
+  root: path.join(__dirname, 'views')
+});
+
+fastify.addHook('preHandler', async (req, res) => {
+  const locatedDocFiles = await findDocFiles();
+  const allPages = locatedDocFiles;
+  const docFilesContent = await getFilesContent(locatedDocFiles);
+  const htmlContent = convertDocToHtml(docFilesContent);
+
+  req.files_data = htmlContent;
+  req.all_pages = allPages;
+});
+
+fastify.get('/', (req, res) => {
   const renderData = {};
-  const specificPageData = res.locals.files_data.find(page => page.name === 'README');
+  const specificPageData = req.files_data.find(page => page.name === 'README');
   renderData.name = (specificPageData) ? specificPageData.name : '/';
   renderData.title = (specificPageData) ? specificPageData.title : 'Home';
   renderData.content = (specificPageData) ? specificPageData.html : adoc.convert(fs.readFileSync(path.resolve('README.adoc'), 'utf-8'), asciidoctorDefaultConfig);
-  renderData.pages = res.locals.all_pages.filter(page => page.name !== 'README');
-  res.render('index', renderData);
+  renderData.pages = req.all_pages.filter(page => page.name !== 'README');
+  res.view('index', renderData);
 });
 
-app.get('/:pageName.html', (req, res) => {
+fastify.get('/:pageName.html', (req, res) => {
   const pageTitle = req.params.pageName;
-  const specificPageData = res.locals.files_data.find(page => page.name === pageTitle);
+  const specificPageData = req.files_data.find(page => page.name === pageTitle);
   if (specificPageData) {
     const content = specificPageData.html;
-    res.render('page', {
+    res.view('page', {
       name: specificPageData.name,
       title: pageTitle,
       content,
       toc: specificPageData.toc,
-      pages: res.locals.all_pages.filter(page => page.name !== 'README')
+      pages: req.all_pages.filter(page => page.name !== 'README')
     });
   } else {
-    res.status(404).render('404', {
+    res.status(404).view('404', {
       name: '404',
       text: errorPage.text,
       title: errorPage.title,
-      pages: res.locals.all_pages.filter(page => page.name !== 'README')
+      pages: req.all_pages.filter(page => page.name !== 'README')
     });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
