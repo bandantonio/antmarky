5e3ea0caacd1dd184c1783b1f46a243c37b1166f
Antonio <zolotuhin87@gmail.com>
2023-12-10 22:25:40 +0200

    refactor(test): saveHtml: switch from mock-fs to native vitest mocks. Add snapshot files. [skip ci]

--- test/unit/saveHtml.test.ts
+++ test/unit/saveHtml.test.ts
@@ -1,76 +1,24 @@
-import fs from 'fs';
-import mock from 'mock-fs';
-import path from 'path';
-import { afterEach, describe, expect, test } from 'vitest';
-import saveHtmlPages from '../../src/saveHtml';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import mockedFilesContents from '../snapshots/mockedFilesContents';
+import * as saveHtmlModule from '../../src/saveHtml';
 
 describe('saveHtml', () => {
-    afterEach(() => {
-        mock.restore();
-    });
-    test('Should save HTML files to the `dist` directory', async () => {
-        mock({
-            'dist': {},
-            'src/templates': mock.load(path.resolve('src/templates'))
+    beforeEach(() => {
+        vi.mock('../../../src/helpers/fileOperations', async () => {
+            const actual = await vi.importActual('../../../src/helpers/fileOperations')
+            return {
+                ...actual!,
+                write: vi.fn(() => Promise.resolve())
+            };
         })
-        let fakeContent = [{
-            fileName: 'index',
-            fileTitle: 'Index',
-            fileDir: process.cwd() + '/docs',
-            fileHtmlContent: '',
-            tableOfContents: []
-        }, {
-            fileName: 'unit-tests',
-            fileTitle: 'Unit-Tests',
-            fileDir: process.cwd() + '/docs',
-            fileHtmlContent: '<div class="sect1">\n' +
-                '<h2 id="heading-for-toc"><a class="anchor" href="#heading-for-toc"></a>Heading for toc</h2>\n' +
-                '<div class="sectionbody">\n' +
-                '<div class="paragraph">\n' +
-                '<p>Hello world from the fake-root-one file</p>\n' +
-                '</div>\n' +
-                '<div class="paragraph">\n' +
-                '<p><a href="https://github.com/bandantonio">link to GitHub</a></p>\n' +
-                '</div>\n' +
-                '</div>\n' +
-                '</div>',
-            tableOfContents: [{
-                id: 'heading-for-toc',
-                level: 1,
-                title: 'Heading for toc'
-            }]
-        }, {
-            fileName: 'asciidoctor',
-            fileTitle: 'Asciidoctor',
-            fileDir: process.cwd() + '/docs',
-            fileHtmlContent: '<div class="paragraph">\n' +
-                '<p>Hello world from the fake-root-two file</p>\n' +
-                '</div>\n' +
-                '<div class="paragraph">\n' +
-                '<p><a href="https://github.com/bandantonio/antmarky">link to Antmarky</a></p>\n' +
-                '</div>',
-            tableOfContents: []
-        }, {
-            fileName: 'unit-tests2',
-            fileTitle: 'Unit-Tests2',
-            fileDir: process.cwd() + '/docs/sub-folder',
-            fileHtmlContent: '<div class="paragraph">\n' +
-                '<p>Hello world from the <strong>fake-root-three file</strong></p>\n' +
-                '</div>\n' +
-                '<div class="paragraph">\n' +
-                '<p><a href="https://github.com/bandantonio/antmarky">link to Antmarky</a></p>\n' +
-                '</div>',
-            tableOfContents: []
-        }];
+    });
+    afterEach(() => {
+        vi.doUnmock('../../../src/helpers/fileOperations');
+    });
+    test(`Should save HTML files to the 'dist' directory`, async () => {
+        vi.spyOn(saveHtmlModule, 'saveHtmlPages');
 
-        await saveHtmlPages(fakeContent);
-
-        const outputFolderContents = await fs.promises.readdir(path.resolve('dist'));
-        expect(outputFolderContents).toEqual([
-            'asciidoctor.html',
-            'index.html',
-            'unit-tests.html',
-            'unit-tests2.html'
-        ]);
+        await saveHtmlModule.saveHtmlPages(mockedFilesContents);
+        expect(saveHtmlModule.saveHtmlPages).toHaveBeenCalledWith(mockedFilesContents);
     });
 });
